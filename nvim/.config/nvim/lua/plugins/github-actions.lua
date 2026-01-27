186aa7fb657d7d685fee02f30cfe1bc70941b65e
Timothy Blamires <tblamires@lucidchart.com>
2026-01-27 16:36:41 -0700

    gh actions lsp

---
+++ nvim/.config/nvim/lua/plugins/github-actions.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        github_actions_ls = {
+          filetypes = { "yaml", "yaml.github" },
+          root_dir = function(fname)
+            local lspconfig_util = require("lspconfig.util")
+            -- Only activate if the file is within a .github directory
+            return fname:match("%.github/") and lspconfig_util.find_git_ancestor(fname)
+          end,
+        },
+      },
+    },
+  },
+}
