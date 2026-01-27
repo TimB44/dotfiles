3b2f0d698793a293d60f143cb51d5c72d7101998
Timothy Blamires <tblamires@lucidchart.com>
2026-01-27 16:58:09 -0700

    fix ts lsp

--- nvim/.config/nvim/lua/plugins/work-typescript.lua
+++ nvim/.config/nvim/lua/plugins/work-typescript.lua
@@ -6,7 +6,12 @@ return {
         ts_ls = {
           init_options = {
             tsserver = {
+<<<<<<< Updated upstream
               path = "/home/tblamires/lucid/main/cake/node_modules/typescript/lib/",
+||||||| Stash base
+=======
+              path = "/var/lucid/main/cake/node_modules/typescript/lib/",
+>>>>>>> Stashed changes
             },
             maxTsServerMemory = 10000,
           },
