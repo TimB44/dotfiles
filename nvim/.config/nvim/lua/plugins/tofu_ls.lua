a7cf6eb61dd7cb6713e96fc0342dabb07a5c8080
Timothy Blamires <tblamires@lucidchart.com>
2026-07-20 00:17:38 -0600

    Update work development configuration

--- nvim/.config/nvim/lua/plugins/tofu_ls.lua
+++ nvim/.config/nvim/lua/plugins/tofu_ls.lua
@@ -1,5 +1,18 @@
 return {
-  cmd = { "tofu-ls", "serve" },
-  filetypes = { "opentofu", "opentofu-vars", "terraform" },
-  root_markers = { ".terraform", ".spacelift", ".lucid_satck_info" },
+  "neovim/nvim-lspconfig",
+  opts = function(_, opts)
+    opts.servers = opts.servers or {}
+    opts.servers.terraformls = {
+      cmd = { "terraform-ls", "serve" },
+      filetypes = { "terraform", "terraform-vars" },
+      root_dir = require("lspconfig.util").root_pattern(".terraform", ".git"),
+      capabilities = {
+        workspace = {
+          didChangeWatchedFiles = {
+            dynamicRegistration = true,
+          },
+        },
+      },
+    }
+  end,
 }
