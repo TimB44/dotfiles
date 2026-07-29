cb3e85bea256107e1711ae024b7dfbd158d12677
Timothy Blamires <tblamires@lucidchart.com>
2026-07-28 20:50:21 -0600

    Sync Neovim yanks over SSH

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- LazyVim disables clipboard syncing over SSH, but tmux forwards OSC 52
+-- sequences through Mosh to the local terminal.
+vim.opt.clipboard = "unnamedplus"
